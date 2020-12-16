981368ed41df41301b6a45755399eeb501d5f1a7
Megharth <m.c.lakhataria@gmail.com>
2020-12-16 01:40:35 -0500

    add ColorsViewProvider.ts

---
+++ src/ColorsViewProvider.ts
@@ -0,0 +1,53 @@
+import * as vscode from 'vscode';
+
+export default class ColorsViewProvider implements vscode.WebviewViewProvider {
+    public readonly viewType:string = 'practice.colorView';
+    private _view?: vscode.WebviewView;
+    private _extensionUri: vscode.Uri;
+
+    constructor(extensionUri: vscode.Uri) {
+        this._extensionUri = extensionUri;
+    }
+
+    public resolveWebviewView(
+        webviewView: vscode.WebviewView,
+        context: vscode.WebviewViewResolveContext,
+        _token: vscode.CancellationToken
+    ) {
+        this._view = webviewView;
+
+        webviewView.webview.options = {
+            enableScripts: true,
+            localResourceRoots: [this._extensionUri]
+        };
+
+        webviewView.webview.html = this._getHTMLContent(webviewView.webview);
+    }
+
+    private _getHTMLContent(webview: vscode.Webview) {
+        const nonce = getNonce();
+        return `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            </head>
+            <body>
+                Hello world!
+            </body>
+        </html>
+        `;
+    }
+
+}
+
+const getNonce = () => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+    return text;
+};
